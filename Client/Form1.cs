49a9fbb4cb796724457e1510db2b66df4f67f0f2
Nathan <nathanyip98@yahoo.co.uk>
2020-03-04 13:12:39 +0000

    test send data method

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -18,17 +18,43 @@ namespace Client
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
-            string _ipAddress = textIP.Text;
-            int _portNumber = Int32.Parse(textPort.Text);
-            client.joinChatroom(_ipAddress,_portNumber);
-            textMessage.Text = "Button is clicked";
-            Console.WriteLine("Button is clicked");
+            try
+            {
+                string _ipAddress = textIP.Text;
+                string _portNumber =textPort.Text;
+                client.joinChatroom(_ipAddress, _portNumber);
+
+                //chatTextbox.AppendText("You have joined the chat room \n\n");
+            }
+            catch (Exception excp)
+            {
+                chatTextbox.AppendText("Cannot connect to the server \n\n");
+            }
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            client.sendMessage(textMessage.Text);
-            textMessage.Text = "";
+            testingMethod();
+
+            //client.sendMessage(textMessage.Text);
+            //textMessage.Text = "";
         }
+
+        private void testingMethod()
+        {
+            TcpClient tc = new TcpClient();
+            tc.Connect("127.0.0.1", 25000);
+
+            NetworkStream ns = tc.GetStream();
+            byte[] data = Encoding.ASCII.GetBytes("Hello");
+
+            ns.Write(data, 0, data.Length);
+        }
+
+        public void appendText(string appendText)
+        {
+            chatTextbox.AppendText(appendText +"\n");
+        }
+
     }
 }
