12c2386eb0c5a52188fe68af0392d5bc361d7cac
Nathan <nathanyip98@yahoo.co.uk>
2020-03-04 10:48:34 +0000

    started ui and code of client

---
+++ Client/ClientSocket.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class ClientSocket
+    {
+        private static Socket client;
+        private IPAddress ipAddress;
+        private int PortNumber;
+
+
+        public ClientSocket()
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        }
+
+        public void Initialize()
+        {
+            // get ip from text box
+
+            // get port number from text box
+
+            // enter button and join chatroom
+
+        }
+
+        private void joinChatroom()
+        {
+
+        }
+
+        private void sendMessage()
+        {
+            string userInput = string.Empty;
+
+            while (true)
+            {
+                // send messages
+
+                userInput = Console.ReadLine();
+                byte[] buffSend = Encoding.ASCII.GetBytes(userInput);
+                client.Send(buffSend);
+
+                // append textbox
+            }
+        }
+
+        private void recieveMessage()
+        {
+            while (true)
+            {
+                // recieve messages
+                byte[] buffReceive = new byte[1024];
+                int nRecv = client.Receive(buffReceive);
+
+                // append textbox
+                //Encoding.ASCII.GetString(buffReceive, 0, nRecv);
+            }
+        }
+
+    }
+}
+
--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textIP = new System.Windows.Forms.TextBox();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.chatTextbox = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textIP
+            // 
+            this.textIP.Location = new System.Drawing.Point(12, 12);
+            this.textIP.Name = "textIP";
+            this.textIP.Size = new System.Drawing.Size(100, 20);
+            this.textIP.TabIndex = 0;
+            this.textIP.Text = "127.0.0.1";
+            // 
+            // textPort
+            // 
+            this.textPort.Location = new System.Drawing.Point(118, 12);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(64, 20);
+            this.textPort.TabIndex = 1;
+            this.textPort.Text = "25000";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(201, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // chatTextbox
+            // 
+            this.chatTextbox.Location = new System.Drawing.Point(13, 52);
+            this.chatTextbox.Multiline = true;
+            this.chatTextbox.Name = "chatTextbox";
+            this.chatTextbox.Size = new System.Drawing.Size(259, 166);
+            this.chatTextbox.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(195, 227);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(12, 229);
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(177, 20);
+            this.textMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.chatTextbox);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.textIP);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textIP;
+        private System.Windows.Forms.TextBox textPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox chatTextbox;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textMessage;
     }
 }
 
