b1f924d9e470f19e892f37678ff324843750d89f
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-03-04 11:34:40 +0000

    Server Update

---
+++ ChatServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+public class ChatServer
+{
+    public TcpListener listenerSocket { get; set; }
+    public List<TcpClient> clientList { get; set; }
+    public IPAddress ipAddress { get; set; }
+    public int port { get; set; }
+    public IPEndPoint ipep { get; set; }
+    public NetworkStream ns { get; set; }
+
+    public ChatServer(int port, string ipAddress)
+	{
+        this.ipAddress = IPAddress.Parse(ipAddress);
+        listenerSocket = new TcpListener(ipAddress, port);
+        listenerSocket.Start();
+        clientList.Add(listenerSocket.AcceptTcpClient());
+	}
+
+    public void broadcast(string msg)
+    {
+        for (int i = clientList.Count; --i > 0;)
+        {
+            ns = clientList[i].GetStream();
+            byte[] data = Encoding.ASCII.GetBytes(msg);
+            ns.Write(data, 0, data.Length);
+        }
+    }
+}
--- Server/Form1.cs
+++ Server/Form1.cs
@@ -5,7 +5,6 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-
 namespace Server
 {
     public partial class Form1 : Form
@@ -17,53 +16,9 @@ namespace Server
 
             private void HostBtn_Click(object sender, EventArgs e)
         {
-
-            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress ipaddr = IPAddress.Any;
-            IPEndPoint ipep = new IPEndPoint(ipaddr, 25000);
-            listenerSocket.Bind(ipep);
-            //int threadCount = 0;
-            while (true)
-            {
-                try
-                {
-                    // Listen for new connections, up to the amount specified as argument
-                    listenerSocket.Listen(5);
-                    // Create a thread to deal with the client
-                    // Accept the connection
-                    Socket client = listenerSocket.Accept();
-                    Console.WriteLine("Client connected: " + client.ToString()
-                        + " - Ip End Point: " + client.RemoteEndPoint.ToString());
-                    // define a buffer as byte array of 128 byte length.
-                    byte[] buff = new byte[128];
-                    // define number of received bytes as an int
-                    int numberOfReceivedBytes = 0;
-
-                    while (true)
-                    {
-                        numberOfReceivedBytes = client.Receive(buff);
-                        Console.WriteLine("Number of received bytes: " +
-                        numberOfReceivedBytes);
-                        //convert from byte array to ascii, using Encoding.ASCII.GetString
-                        string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
-                        //print out text
-                        Console.WriteLine("Data sent by client is: " + receivedText);
-                        //Send data back to sender
-                        client.Send(buff);
-                        if (receivedText == "x")
-                        {
-                            listenerSocket.Close();
-                            break;
-                        }
-                        Array.Clear(buff, 0, buff.Length);
-                        numberOfReceivedBytes = 0;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-            }
+            string ipAddress = IPTxt.Text;
+            int port = int.Parse(PortTxt.Text);
+            ChatServer server = new ChatServer(port, ipAddress);
         }
     }
 }
